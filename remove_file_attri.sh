ae97ea0b1445d0bed15986355bffa45fe8647d99
YuBing <icedfish@gmail.com>
2012-08-12 23:49:47 +0800

    add some notes

--- remove_file_attri.sh
+++ remove_file_attri.sh
@@ -2,19 +2,23 @@
 
 #########################################################
 # Author:	YuBing <icedfish@gmail.com>
+#
 # Usage:
 #		1. Single File:
 #			bash this_script file_dir
 #		2. A Whole Folder（非递归）:
 #			ls -l@ folder_dir | grep rw | grep '@ ' |awk '{print $NF}' | bash this_script
+#
 # Note:		用于清除Mac系统给文件上面加的各种多余属性
 #			常见两种问题：
 #				1. 打开软件的时候提示应用程序是互联网下载的，可能不安全
 #					每次打开需要点击确认。
 #				2. NTFS的移动硬盘上有些文件在mac下面显示灰色，无法打开。
 # 			http://en.wikipedia.org/wiki/Extended_file_attributes
+#
 # Version:	0.3
 # Last Modify: 2012-8-12
+# Tested: Mac OS X - Mountain Lion
 #########################################################
 
 
