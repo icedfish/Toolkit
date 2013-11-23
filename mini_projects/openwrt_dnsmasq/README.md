998d58d5e627922a27c02a4f19dd47a5e6959ae7
YuBing <icedfish@gmail.com>
2013-11-23 22:15:25 +0800

    更新实现，改为用效率更高的hosts方式，同时增加了单独整理好的视频网站需要屏蔽的域名

---
+++ mini_projects/openwrt_dnsmasq/README.md
@@ -0,0 +1,9 @@
+##说明
+通过在路由器上统一过滤各种广告站点，进而实现家庭局域网内统一屏蔽广告的目的。
+
+## Tested 
+openwrt 3.10.13 + Buffalo WHR-G301N 
+
+##主要屏蔽以下几类：
+1. adblock上的 (disabled_simple_hosts)
+2. 视频网站广告 (video_sites.conf)
---
+++ mini_projects/openwrt_dnsmasq/update_dnsmasq.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+#脚本原理: 
+#从adblock-chinalist下载广告网址
+#dnsmasq拦截将广告网址IP定义为127.0.0.1地址
+#重启dnsmasq
+
+# crontab:
+#0 0 * * * /bin/sh /etc/dnsmasq/update_dnsmasq.sh
+
+# v1: use pure dnsmasq format config #
+# v1 代码来源于互联网，原始作者未考证#
+#wget -O - http://adblock-chinalist.googlecode.com/svn/trunk/adblock-lazy.txt | grep ^\|\|[^\*]*\^$ | sed -e 's:||:address\=\/:' -e 's:\^:/127\.0\.0\.1:' > /etc/dnsmasq.ads
+#grep conf-file /etc/dnsmasq.conf || echo -e "\nconf-file=/etc/dnsmasq.ads" >> /etc/dnsmasq.conf
+#/etc/init.d/dnsmasq restart
+
+# v2: use hosts like files #
+# v2 比v1 ：大幅节省空间占用和效率(路由器的硬件比较弱)
+mkdir -p /etc/dnsmasq
+
+wget -O - http://adblock-chinalist.googlecode.com/svn/trunk/adblock-lazy.txt | grep ^\|\|[^\*]*\^$ |sed -e 's#||##' -e 's#\^##'| tr "\n" " " |sed -e "s#^#127.0.0.1 #" > /etc/dnsmasq/disabled_simple_hosts
+
+grep addn-hosts /etc/dnsmasq.conf || echo -e "\naddn-hosts=/etc/dnsmasq/disabled_simple_hosts" >> /etc/dnsmasq.conf
+
+/etc/init.d/dnsmasq restart
