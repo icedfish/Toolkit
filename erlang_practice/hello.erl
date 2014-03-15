ffb13d1834083b7df32c27d8f2ef0628c98b0657
YuBing <icedfish@gmail.com>
2014-03-15 21:56:24 +0800

    erlang day2

--- erlang_practice/hello.erl
+++ erlang_practice/hello.erl
@@ -1,4 +1,10 @@
 -module(hello).
 -export([say/1]).
 
-say(Anything) -> Anything.
+say(Anything) -> private_say(Anything).
+
+% 没有export的方法，是module内的私有方法。
+private_say(Sth) -> Sth.
+
+% 列表处理语法糖
+%  [{Item, Quantity * Price} || {Item, Quantity, Price} <- [{ book, 31, 4.5 }, { food, 50, 1 }]].
--- erlang_practice/math.erl
+++ erlang_practice/math.erl
@@ -1,14 +1,14 @@
 -module(math).  %往现有的math module上增加新的函数。
 -export([fib/1, factorial/1]).
 
-fib(N) when (N < 0) orelse (not is_integer(N)) -> 'error input!' ;
-fib(0) -> 0 ;
-fib(1) -> 1 ;
-fib(N) when N > 1 -> fib(N-2) + fib(N-1).
+fib(N) when (N =< 0) orelse (not is_integer(N)) -> 'error input!' ;
+fib(N) when N > 1 -> fib(N-2) + fib(N-1);
+fib(1) -> 1 .
 
 
-factorial(N) when (N <= 0) orelse (not is_integer(N)) -> 'error input!' ;
+factorial(N) when (N =< 0) orelse (not is_integer(N)) -> 'error input!' ;
 factorial(1) -> 1 ;
 factorial(N) -> factorial(N-1) * N.
+%最后这两行倒换效率是否有差异？ Guard计算效率高还是Clause比对效率高？第一行在大量计算时候是否降低效率？ 这些都需要继续深入研究。
 
 %@todo 两个函数都需要做尾递归优化
