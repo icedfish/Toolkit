b43401b3088a3cdf6a7a74e1e2d77a561bd8f02f
YuBing <icedfish@gmail.com>
2014-05-14 20:31:03 +0800

    script to enable jmx support for es

--- erlang_practice/demo.erl
+++ erlang_practice/demo.erl
@@ -1,12 +1,15 @@
--module(demo).
--export([ttt/1]).
-% PRACTICE FOR GAME tic-tac-toe
-% @todo 如何实现方法内部的常量定义？
+-module(translate).
+-export([loop/0]).
 
-%List = [x,o,o, x,o,free, x,x,o].
-
-ttt(List) when any(free, List) -> continue;
-ttt(List) -> 
-	
-good	%任意三个，距离为1的格子内容相同，记为胜，否则为未知胜负
-.
+loop() ->
+    receive
+            "dong" ->
+                        io:format("house~n" ),
+                        loop();
+            "blanca" ->
+                        io:format("white~n" ),
+                        loop();        
+            _ ->            
+            			io:format("I don't understand.~n" ),
+            			loop()
+end.
---
+++ es/add_VisualVM_for_ES.md
@@ -0,0 +1,21 @@
+
+from: http://www.elasticsearch.org/guide/en/elasticsearch/reference/0.90/modules-jmx.html
+
+## @elasticsearch.yml
+
+```
+jmx.create_connector: true
+```
+
+## @elasticsearch.in.sh
+
+
+# ensures JMX accessible from outside world
+
+```
+JAVA_OPTS="$JAVA_OPTS -Dcom.sun.management.jmxremote.ssl=false"
+JAVA_OPTS="$JAVA_OPTS -Dcom.sun.management.jmxremote.authenticate=false"
+JAVA_OPTS="$JAVA_OPTS -Djava.rmi.server.hostname=$(ifconfig|grep 'inet addr:'|grep -v '127.0.0.1'|cut -d: -f2 | awk '{ print $1}')"
+```
+
+如果启动时候出现 java.net.MalformedURLException: Local host name unknown:xxx ,那么确保hostname可以解析到这个ip。
