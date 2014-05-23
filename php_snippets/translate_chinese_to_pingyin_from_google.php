c866ddda64d202cb69bb256d29aaecbd8fc194b8
YuBing <icedfish@gmail.com>
2014-05-23 13:03:48 +0800

    修正了带‘的注音问题

--- php_snippets/translate_chinese_to_pingyin_from_google.php
+++ php_snippets/translate_chinese_to_pingyin_from_google.php
@@ -5,11 +5,11 @@
  * 常见的直接翻译的类库都太暴力了，多音词很难识别，找了一圈，发现还是Google翻译靠谱，就用他了！
  *
  * @param $chinese_text
- * @param bool $use_four_tones 是否反馈带中文注音音调的拼音
+ * @param bool $use_origin 返回带声调的标准拼音
  * @return string
  * @throws Exception
  */
-function pinyin($chinese_text, $use_four_tones = false) {
+function pinyin($chinese_text, $use_origin = false) {
 	$target_url = 'http://translate.google.cn/translate_a/t?client=t&sl=zh-CN&tl=zh-CN&hl=en&sc=2&ie=UTF-8&oe=UTF-8&prev=btn&rom=1&srcrom=1&ssel=4&tsel=3&q=';
 
 	$rt = file_get_contents($target_url . urlencode($chinese_text) . '&time=' . time());
@@ -26,7 +26,7 @@ function pinyin($chinese_text, $use_four_tones = false) {
 	preg_match("/^\[\[\[([^\]]+)\]\]/", $rt, $matches);
 	$pinyin = mb_strtolower(trim(explode(',', $matches[1])[2], '"'));
 
-	if ($use_four_tones) {
+	if ($use_origin) {
 		return $pinyin;
 	} else {
 		$replace = [
@@ -37,10 +37,13 @@ function pinyin($chinese_text, $use_four_tones = false) {
 			'ū' => 'u', 'ú' => 'u', 'ǔ' => 'u', 'ù' => 'u',
 			'ǖ' => 'v', 'ǘ' => 'v', 'ǚ' => 'v', 'ǜ' => 'v',
 		];
-
+		$pinyin = str_replace("'", " ", $pinyin); // 去掉多余的单引号，比如“四阿哥” => "Sì'ā gē"
+		//@todo 拆分连接词，比如shíjiān，最好返回 "shi jian", 现在返回的是 "shijian"
 		return strtr($pinyin, $replace);
 	}
 
 }
 
-// sample: echo pinyin('中国');
+// sample: 
+//echo pinyin('四阿哥 长大需要长很长时间');
+//echo pinyin('iphone');
