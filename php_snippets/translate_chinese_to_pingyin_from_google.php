7ae276aebe29c530b3841f7e3ae9ed3782db2f8b
YuBing <icedfish@gmail.com>
2014-05-26 23:04:59 +0800

    解决“不能”错译成‘bun eng’的问题

--- php_snippets/translate_chinese_to_pingyin_from_google.php
+++ php_snippets/translate_chinese_to_pingyin_from_google.php
@@ -40,23 +40,15 @@ function pinyin($chinese_text, $use_origin = false) {
 		$pinyin = str_replace("'", " ", $pinyin); // 去掉多余的单引号，比如“四阿哥” => "Sì'ā gē"
 		$pinyin = strtr($pinyin, $replace);
 		
-		//see: http://zh.wikipedia.org/w/index.php?title=%E9%9F%B5%E6%AF%8D&variant=zh-cn
-		$yunmu = [
-			'a', 'o', 'e', 'i', 'u', 'v',
-			'er', 'ai', 'ei', 'ao', 'ou',
-			'an', 'ang', 'en', 'eng', 'in', 'ing',  
-			'ia', 'io', 'ie', 'ua', 'uo', 've',
-			'iai', 'iao', 'iou', 'ian', 'iang', 'iong',
-			'uai', 'uei', 'uan', 'uen', 'uang', 'ueng', 'ong',
-			'un','van', 'ue',
-		];
+		$yunmu = "a|ai|an|ang|ao|ba|bai|ban|bang|bao|bei|ben|beng|bi|bian|biao|bie|bin|bing|bo|bu|ca|cai|can|cang|cao|ce|ceng|cha|chai|chan|chang|chao|che|chen|cheng|chi|chong|chou|chu|chuai|chuan|chuang|chui|chun|chuo|ci|cong|cou|cu|cuan|cui|cun|cuo|da|dai|dan|dang|dao|de|deng|di|dian|diao|die|ding|diu|dong|dou|du|duan|dui|dun|duo|e|en|er|fa|fan|fang|fei|fen|feng|fo|fou|fu|ga|gai|gan|gang|gao|ge|gei|gen|geng|gong|gou|gu|gua|guai|guan|guang|gui|gun|guo|ha|hai|han|hang|hao|he|hei|hen|heng|hong|hou|hu|hua|huai|huan|huang|hui|hun|huo|ji|jia|jian|jiang|jiao|jie|jin|jing|jiong|jiu|ju|juan|jue|jun|ka|kai|kan|kang|kao|ke|ken|keng|kong|kou|ku|kua|kuai|kuan|kuang|kui|kun|kuo|la|lai|lan|lang|lao|le|lei|leng|li|lia|lian|liang|liao|lie|lin|ling|liu|long|lou|lu|lv|luan|lue|lun|luo|ma|mai|man|mang|mao|me|mei|men|meng|mi|mian|miao|mie|min|ming|miu|mo|mou|mu|na|nai|nan|nang|nao|ne|nei|nen|neng|ni|nian|niang|niao|nie|nin|ning|niu|nong|nu|nv|nuan|nue|nuo|o|ou|pa|pai|pan|pang|pao|pei|pen|peng|pi|pian|piao|pie|pin|ping|po|pu|qi|qia|qian|qiang|qiao|qie|qin|qing|qiong|qiu|qu|quan|que|qun|ran|rang|rao|re|ren|reng|ri|rong|rou|ru|ruan|rui|run|ruo|sa|sai|san|sang|sao|se|sen|seng|sha|shai|shan|shang|shao|she|shen|sheng|shi|shou|shu|shua|shuai|shuan|shuang|shui|shun|shuo|si|song|sou|su|suan|sui|sun|suo|ta|tai|tan|tang|tao|te|teng|ti|tian|tiao|tie|ting|tong|tou|tu|tuan|tui|tun|tuo|wa|wai|wan|wang|wei|wen|weng|wo|wu|xi|xia|xian|xiang|xiao|xie|xin|xing|xiong|xiu|xu|xuan|xue|xun|ya|yan|yang|yao|ye|yi|yin|ying|yo|yong|you|yu|yuan|yue|yun|za|zai|zan|zang|zao|ze|zei|zen|zeng|zha|zhai|zhan|zhang|zhao|zhe|zhen|zheng|zhi|zhong|zhou|zhu|zhua|zhuai|zhuan|zhuang|zhui|zhun|zhuo|zi|zong|zou|zu|zuan|zui|zun|zuo";
 
 		$arr_yunmu = [];
-		foreach($yunmu as $_ym) {
+		foreach(explode('|', $yunmu) as $_ym) {
 			$arr_yunmu[$_ym] = "{$_ym} ";
 		}
+
 		//拆分连词，比如shíjiān，最好返回 "shi jian", google返回的是 "shijian"
-		//@todo 现在这种实现还是有问题，"不能" 会返回 "bun eng" 而不是 "bu neng".
+		//已解决问题： "不能" 返回 "bu neng" 而不是 "bun eng".
 		$pinyin = str_replace("  ", " ", strtr($pinyin, $arr_yunmu));
 		
 		return $pinyin;
