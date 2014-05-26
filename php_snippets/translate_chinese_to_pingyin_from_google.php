97672d2cc77f0359f442faabd308a0b650857418
YuBing <icedfish@gmail.com>
2014-05-26 22:56:18 +0800

    尝试解决连词问题，但是现在的方法还是不够好。

--- php_snippets/translate_chinese_to_pingyin_from_google.php
+++ php_snippets/translate_chinese_to_pingyin_from_google.php
@@ -14,7 +14,7 @@ function pinyin($chinese_text, $use_origin = false) {
 
 	$rt = file_get_contents($target_url . urlencode($chinese_text) . '&time=' . time());
 
-	if (!mb_strpos($rt, $chinese_text)) {
+	if (mb_strpos($rt, '[[[') !== 0) {
 		throw new Exception("Fail to get data from google");
 	}
 
@@ -23,8 +23,8 @@ function pinyin($chinese_text, $use_origin = false) {
 		$rt = '[[["四阿哥 长大需要长很长时间","四阿哥 长大需要长很长时间","Sì'ā gē zhǎng dà xūyào zhǎng hěn cháng shíjiān","Sì'ā gē zhǎng dà xūyào zhǎng hěn cháng shíjiān"]],,"zh-CN",,[["四阿哥 长大需要长很长时间",,false,false,0,0,0,0]],,,,[["zh-CN","zh-CN"]],4]';
 	*/
 
-	preg_match("/^\[\[\[([^\]]+)\]\]/", $rt, $matches);
-	$pinyin = mb_strtolower(trim(explode(',', $matches[1])[2], '"'));
+	preg_match("/^\[\[\[([^\]]+)\]/", $rt, $matches);
+	$pinyin = mb_strtolower(explode('","', $matches[1])[2]);
 
 	if ($use_origin) {
 		return $pinyin;
@@ -38,8 +38,28 @@ function pinyin($chinese_text, $use_origin = false) {
 			'ǖ' => 'v', 'ǘ' => 'v', 'ǚ' => 'v', 'ǜ' => 'v',
 		];
 		$pinyin = str_replace("'", " ", $pinyin); // 去掉多余的单引号，比如“四阿哥” => "Sì'ā gē"
-		//@todo 拆分连接词，比如shíjiān，最好返回 "shi jian", 现在返回的是 "shijian"
-		return strtr($pinyin, $replace);
+		$pinyin = strtr($pinyin, $replace);
+		
+		//see: http://zh.wikipedia.org/w/index.php?title=%E9%9F%B5%E6%AF%8D&variant=zh-cn
+		$yunmu = [
+			'a', 'o', 'e', 'i', 'u', 'v',
+			'er', 'ai', 'ei', 'ao', 'ou',
+			'an', 'ang', 'en', 'eng', 'in', 'ing',  
+			'ia', 'io', 'ie', 'ua', 'uo', 've',
+			'iai', 'iao', 'iou', 'ian', 'iang', 'iong',
+			'uai', 'uei', 'uan', 'uen', 'uang', 'ueng', 'ong',
+			'un','van', 'ue',
+		];
+
+		$arr_yunmu = [];
+		foreach($yunmu as $_ym) {
+			$arr_yunmu[$_ym] = "{$_ym} ";
+		}
+		//拆分连词，比如shíjiān，最好返回 "shi jian", google返回的是 "shijian"
+		//@todo 现在这种实现还是有问题，"不能" 会返回 "bun eng" 而不是 "bu neng".
+		$pinyin = str_replace("  ", " ", strtr($pinyin, $arr_yunmu));
+		
+		return $pinyin;
 	}
 
 }
@@ -47,3 +67,4 @@ function pinyin($chinese_text, $use_origin = false) {
 // sample: 
 //echo pinyin('四阿哥 长大需要长很长时间');
 //echo pinyin('iphone');
+//echo pinyin('我有一个美国朋友，他想学汉语，可是一点基础都没有，必须从拼音开始学起，由于我们现在都没有时间，也不能经常见面，所以我现在没办法直接教他，希望有人能帮我提供些好的素材，尤其是拼音发声练习方面的，可以暂时供他自己模仿练习用，我从土豆网也下载过类似视频，可是都不完整，而且声音很不清楚，所以希望大家能给我提供些效果很好的材料，谢谢大家');
