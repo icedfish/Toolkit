443de031ca814011aed451e7fa72e1192bb13cb8
Yu Bing <yubing@wutongwan.org>
2016-07-28 13:34:24 +0800

    add ik & navel config

--- es/es_install_plugin.sh
+++ es/es_install_plugin.sh
@@ -1,9 +1,23 @@
 ##安装ES常用插件脚本
-cd /usr/share/elasticsearch/
+cd /usr/share/elasticsearch/  #for linux yum installed
+cd /usr/local/Cellar/elasticsearch/2.3.4/libexec/  #for mac brew installed
 
-bin/plugin --install mobz/elasticsearch-head
-bin/plugin --install lukas-vlcek/bigdesk
-bin/plugin --install royrusso/elasticsearch-HQ
-bin/plugin --install lmenezes/elasticsearch-kopf
+bin/plugin install mobz/elasticsearch-head
+bin/plugin install lmenezes/elasticsearch-kopf
+bin/plugin install royrusso/elasticsearch-HQ
 
-bin/plugin -i elasticsearch/elasticsearch-repository-hdfs/2.0.1
+# install IK
+ver="1.9.4"
+mkdir plugins/ik
+cd plugins/ik
+wget https://github.com/medcl/elasticsearch-analysis-ik/releases/download/v$ver/elasticsearch-analysis-ik-$ver.zip
+unzip elasticsearch-analysis-ik-$ver.zip
+rm -f elasticsearch-analysis-ik-$ver.zip
+
+
+# Mavel Agent
+bin/plugin install license
+bin/plugin install marvel-agent
+
+#install kibana first
+/opt/kibana/bin/kibana plugin --install elasticsearch/marvel/latest
